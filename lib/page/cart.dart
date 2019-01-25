46e051f6a34841143a4d8ff48e029bbf78b6dfdc
Swifty Wang <swiftywang@m800.com>
2019-01-25 18:40:14 +0800

    add pull to refresh widget

--- lib/page/cart.dart
+++ lib/page/cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
 
 typedef void CartCreateCallBack(CartController controller);
 
@@ -85,19 +86,49 @@ class InternalCartPage extends StatefulWidget {
 }
 
 class _InternalCartPageState extends State<InternalCartPage> {
+  List<String> addStr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  List<String> str = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  GlobalKey<EasyRefreshState> _easyRefreshKey =
+      new GlobalKey<EasyRefreshState>();
+
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text('flutter cart call from Android'),
+    return new EasyRefresh(
+      key: _easyRefreshKey,
+      autoLoad: false,
+      behavior: ScrollOverBehavior(),
+      child: new ListView.builder(
+          //ListView的Item
+          itemCount: str.length,
+          itemBuilder: (BuildContext context, int index) {
+            return new Container(
+                height: 70.0,
+                child: Card(
+                  child: new Center(
+                    child: new Text(
+                      str[index],
+                      style: new TextStyle(fontSize: 18.0),
+                    ),
+                  ),
+                ));
+          }),
+      onRefresh: () async {
+        await new Future.delayed(const Duration(seconds: 1), () {
+          setState(() {
+            str.clear();
+            str.addAll(addStr);
+          });
+        });
+      },
+      loadMore: () async {
+        await new Future.delayed(const Duration(seconds: 1), () {
+          if (str.length < 20) {
+            setState(() {
+              str.addAll(addStr);
+            });
+          }
+        });
+      },
     );
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text('购物车'),
-//        centerTitle: true,
-//      ),
-//      body: new Center(
-//        child: new Text('flutter cart call from Android'),
-//      ),
-//    );
   }
 }
