c520f959a0d1395b0f56677aedad60a362ea3e37
Swifty Wang <swiftywang@m800.com>
2019-01-28 12:12:14 +0800

    fine tune pull to refresh

--- lib/page/cart.dart
+++ lib/page/cart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_easyrefresh/phoenix_header.dart';
+import 'package:flutter_easyrefresh/phoenix_footer.dart';
 
 typedef void CartCreateCallBack(CartController controller);
 
@@ -80,33 +82,45 @@ class ListWidget extends StatelessWidget {
   }
 }
 
-class InternalCartPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new _InternalCartPageState();
-}
-
 class _InternalCartPageState extends State<InternalCartPage> {
-  List<String> addStr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  List<String> str = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  List<int> datas = List();
+  int page = 0;
+
+  GlobalKey<RefreshHeaderState> _headerKey =
+      new GlobalKey<RefreshHeaderState>();
+  GlobalKey<RefreshFooterState> _footerKey =
+      new GlobalKey<RefreshFooterState>();
   GlobalKey<EasyRefreshState> _easyRefreshKey =
       new GlobalKey<EasyRefreshState>();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _easyRefreshKey.currentState.callRefresh();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new EasyRefresh(
       key: _easyRefreshKey,
-      autoLoad: false,
+      autoLoad: true,
+      refreshHeader: new PhoenixHeader(key: _headerKey),
+      refreshFooter: new PhoenixFooter(key: _footerKey),
       behavior: ScrollOverBehavior(),
       child: new ListView.builder(
           //ListView的Item
-          itemCount: str.length,
+          itemCount: datas.length,
           itemBuilder: (BuildContext context, int index) {
             return new Container(
                 height: 70.0,
                 child: Card(
                   child: new Center(
                     child: new Text(
-                      str[index],
+                      datas[index].toString(),
                       style: new TextStyle(fontSize: 18.0),
                     ),
                   ),
@@ -115,20 +129,38 @@ class _InternalCartPageState extends State<InternalCartPage> {
       onRefresh: () async {
         await new Future.delayed(const Duration(seconds: 1), () {
           setState(() {
-            str.clear();
-            str.addAll(addStr);
+            datas.clear();
+            datas.addAll(genData(page = 0));
           });
         });
       },
       loadMore: () async {
         await new Future.delayed(const Duration(seconds: 1), () {
-          if (str.length < 20) {
-            setState(() {
-              str.addAll(addStr);
-            });
-          }
+          setState(() {
+            datas.addAll(genData(++page));
+          });
         });
       },
     );
   }
+
+  List<int> genData(int i) {
+    return [
+      0 + i * 10,
+      1 + i * 10,
+      2 + i * 10,
+      3 + i * 10,
+      4 + i * 10,
+      5 + i * 10,
+      6 + i * 10,
+      7 + i * 10,
+      8 + i * 10,
+      9 + i * 10,
+    ];
+  }
+}
+
+class InternalCartPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _InternalCartPageState();
 }
